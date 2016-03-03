f88f76d50d1f737a00b5e58ae4a55d6777cde8ad
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-03-03 11:10:48 -0600

    changes

--- reusableTemplate.js
+++ reusableTemplate.js
@@ -1,82 +1,159 @@
-var library = (function(){
-    
-	return {
-		// Utility --- Complete Functions Below
-		identity : function(val) {
+function shuffle(array) {
+ var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+
+var library = (function () {
+
+    return {
+        // Utility --- Complete Functions Below
+        identity: function (val) {
             return val;
-            
+
         },
 
-		// Collections --- Complete Functions Below
-		each : function(list, iterator) {
-            var arr = []; 
+        // Collections --- Complete Functions Below
+        each: function (list, iterator) {
+            var arr = [];
             for (var index = 0; index < list.length; index++) {
                 var element = list[index];
                 var insert = [];
-                insert.push(element,index,list);
-                arr.push(insert);       
+                insert.push(element, index, list);
+                arr.push(insert);
             }
 
         },
 
-		filter : function(list, test) {},
+        filter: function (animals, test) {
+            var newArr = [];
+            for (var index = 0; index < animals.length; index++) {
+                var element = animals[index];
+                if (test(element)) {
+                    newArr.push(element);
+                }
+            }
+            return newArr;
+        },
 
-		reject : function(list, test) {},
+        reject: function (list, test) {
+            var newArr = [];
+            for (var index = 0; index < list.length; index++) {
+                var element = list[index];
+                if (!test(element)) {
+                    newArr.push(element);
+                }
+            }
+            return newArr;
+        },
 
-		map : function(list, iterator) {},
+        map: function (list, iterator) {
+            var arr = [];
+            list.forEach(function (iterator) {
+                arr.push(iterator);
+            }, this);
+            return arr;
+        },
 
-		pluck : function(list, key) {
-			return this.map(list, function(item){
-				return item[key];
-			});
-		},
-		reduce : function(list, iterator, accumulator) {},
+        pluck: function (list, key) {
+            return this.map(list, function (item) {
+                return item[key];
+            });
+        },
+        reduce: function (list, iterator, accumulator) {
+            list.forEach(function (iterator) {
+                accumulator = accumulator + iterator;
+            }, this);
+            return accumulator;
+        },
 
-		every : function(list, iterator) {},
+        every: function (list, iterator) {
+            list.forEach(function (element) {
+                if (iterator(element)) {
+                    return false;
+                }
+            }, this);
 
-		some : function(list, iterator) {},
+            return true;
+        },
 
-		contains : function(list, target) {},
+        some: function (list, iterator) {
+            list.forEach(function (element) {
+                if (iterator(element)) {
+                    return true;
+                }
+            }, this);
 
-		// Advanced Collections --- Complete Functions Below
-		shuffle : function(array) {},
+            return false;
+        },
 
-		invoke : function(list, methodName, args) {},
+        contains: function (list, target) {
+            var exists = false
+            for (var index = 0; index < list.length; index++) {
+                var element = list[index];
+                if (element == target) {
+                    exists = true;
+                }
+            }
+            return exists;
+        },
 
-		sortBy : function(list, iterator) {},
+        // Advanced Collections --- Complete Functions Below
+        shuffle: function (array) {
+            var finalArr = shuffle(array);
 
-		// Objects --- Complete Functions Below
-		extend : function(obj) {},
+            return finalArr;
 
-		defaults : function(obj) {},
+        },
 
-		// Arrays --- Complete Functions Below
-		first : function(array, n) {
-			return n === undefined ? array[0] : array.slice(0, n);
-		},
+        invoke: function (list, methodName, args) { 
+            for (var index = 0; index < list.length; index++) {
+                var element = list[index];
+                 methodName.apply(element, list); 
+            }   
+        },
 
-		last : function(array, n) {},
+        sortBy: function (list, iterator) { },
 
-		indexOf : function(array, target){},
+        // Objects --- Complete Functions Below
+        extend: function (obj) { },
 
-		uniq : function(array) {},
+        defaults: function (obj) { },
 
-		// Advanced Arrays --- Complete Functions Below
-		zip : function() {},
+        // Arrays --- Complete Functions Below
+        first: function (array, n) {
+            return n === undefined ? array[0] : array.slice(0, n);
+        },
 
-		flatten : function(nestedArray, result) {},
+        last: function (array, n) { },
 
-		intersection : function() {},
+        indexOf: function (array, target) { },
 
-		difference : function(array) {},
+        uniq: function (array) { },
 
-		// Functions --- Complete Functions Below
-		once : function(func) {},
+        // Advanced Arrays --- Complete Functions Below
+        zip: function () { },
 
-		memoize : function(func) {},
+        flatten: function (nestedArray, result) { },
 
-		delay : function(func, wait) {}
-	}
+        intersection: function () { },
+
+        difference: function (array) { },
+
+        // Functions --- Complete Functions Below
+        once: function (func) { },
+
+        memoize: function (func) { },
+
+        delay: function (func, wait) { }
+    }
 })();
 
 
